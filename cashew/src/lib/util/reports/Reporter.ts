7b87aae4ebc5884f2b183d5f8f34e6edce65f064
MDLeide <mdleide@gmail.com>
2018-03-01 09:50:08 -0500

    formatting fixes

--- cashew/src/lib/util/reports/Reporter.ts
+++ cashew/src/lib/util/reports/Reporter.ts
@@ -1,6 +1,9 @@
 import { Empire } from "../../empire/Empire";
+import { Colony } from "../../colony/Colony";
+
 import { OperationReport } from "./OperationReport";
 import { Operation } from "../../operation/Operation";
+import { StringBuilder } from "../StringBuilder";
 
 export class Reporter {
     constructor(public empire: Empire) { }
@@ -15,24 +18,29 @@ export class Reporter {
         var html = "";
         for (var i = 0; i < this.empire.colonies.length; i++) {
             html += this.colonyOperations(this.empire.colonies[i].name);
-            if (i != this.empire.colonies.length)
+            if (i != this.empire.colonies.length - 1)
                 html += "</br></br>";
         }
         return html;
     }
 
-    public colonyOperations(colonyName: string): string {
-        for (var i = 0; i < this.empire.colonies.length; i++) {
-            var colony = this.empire.colonies[i];
-            if (colony.name == colonyName) {
-                var html = "";
-                for (var i = 0; i < colony.operationPlans.length; i++) {
-                    html += this.printOperations(colony.operationPlans[i].operationGroup.operations);
-                }
-                return html;
-            }
+    public colonyOperations(colony: (string | Colony)): string {
+        if (!(colony instanceof Colony)) {
+            return this.colonyOperations(this.empire.getColonyByName(colony))
         }
-        return "";
+        if (!colony)
+            return "";
+
+        let sb = new StringBuilder();
+        for (var i = 0; i < colony.operationPlans.length; i++) {
+            sb.appendLine(colony.operationPlans[i].type, "orange");
+            sb.appendLine();
+
+            sb.append(this.printOperations(colony.operationPlans[i].operationGroup.operations));
+            if (i != colony.operationPlans.length - 1)
+                sb.appendLine();
+        }
+        return sb.getString();
     }
 
     private printOperations(operations: Operation[]): string {
