c67128b1cb80f3c222876384052e681a76045e8c
MDLeide <mdleide@gmail.com>
2018-03-02 14:20:35 -0500

    clean up memory

--- cashew/src/memory.d.ts
+++ cashew/src/memory.d.ts
@@ -2,10 +2,6 @@ import { Execute } from "./imp/Execution";
 
 declare global {
     interface Memory {
-        containers: { [containerId: string]: ContainerMemory };
-        controllers: { [controllerId: string]: ControllerMemory };
-        sources: { [sourceId: string]: SourceMemory };
-
         visuals: VisualsMemory;
         empire: EmpireMemory;
     }
--- cashew/src/screeps-memory.d.ts
+++ cashew/src/screeps-memory.d.ts
@@ -9,32 +9,6 @@ declare global {
         deathTick: number;
     }
 
-    interface ContainerMemory {
-        id: string;
-        /*Describes the usage of this container.*/
-        tag: string;
-        /*Holds the ID of the object this container supports.*/
-        tagId: string;
-    }
-
-    interface RoomMemory {
-
-    }
-
-    interface SourceMemory {
-        id: string; //todo: remove
-        isBeingHarvested: boolean;
-    }
-
-    interface SpawnMemory {
-
-    }
-
-    interface ControllerMemory {
-        id: string;
-        containerId: string;
-    }
-
     interface FlagMemory {
         colonyData?: ColonyFlagMemory;
         playbackData?: PlaybackFlagMemory;
